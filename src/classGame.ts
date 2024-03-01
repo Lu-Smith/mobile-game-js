0ffe1c46052d2e0294356d99c57057b0a8c52e18
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-01 19:25:06 +0000

    Updated classGame.ts

--- src/classGame.ts
+++ src/classGame.ts
@@ -4,10 +4,13 @@ export default class Game {
     width: number;
     height: number;
 
-    constructor(canvas, context) {
+    constructor(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) {
         this.canvas = canvas;
         this.context = context;
         this.width = this.canvas.width;
         this.height = this.canvas.height;
+    }
+    render() {
+        this.context.fillRect(100, 100, 50, 150);
     }
 }
