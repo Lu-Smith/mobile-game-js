0667f6fc8dcb325e01c6a47831061b4ed643206d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-01 19:42:50 +0000

    Updated classGame.ts

--- src/classGame.ts
+++ src/classGame.ts
@@ -1,16 +1,21 @@
+import Player from './player';
+
 export default class Game {
     canvas: HTMLCanvasElement;
     context: CanvasRenderingContext2D;
     width: number;
     height: number;
+    player: Player;
 
     constructor(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) {
         this.canvas = canvas;
         this.context = context;
         this.width = this.canvas.width;
         this.height = this.canvas.height;
+        this.player = new Player(this);
     }
     render() {
-        this.context.fillRect(100, 100, 50, 150);
+        this.context.fillStyle = 'red';
+        this.player.draw();
     }
 }
