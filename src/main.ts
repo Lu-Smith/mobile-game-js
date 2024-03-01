16877d1df00603b86a7d4599786bc164807956ce
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-01 19:07:52 +0000

    Updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ import './style.css'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    Hello World
-  </div>
+    <canvas id="canvas1"></canvas>
 `
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
