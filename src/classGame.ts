38f682fb60ca60fad04fbba4f2fb803f033d45d4
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-01 20:06:43 +0000

    Updated classGame.ts

--- src/classGame.ts
+++ src/classGame.ts
@@ -13,9 +13,23 @@ export default class Game {
         this.width = this.canvas.width;
         this.height = this.canvas.height;
         this.player = new Player(this);
+
+        this.resize(window.innerWidth, window.innerHeight);
+
+        window.addEventListener('resize', e => {
+            console.log(e.currentTarget.innerWidth);
+            this.resize(e.currentTarget.innerWidth, e.currentTarget.innerHeight)       
+        })
+    }
+    resize(width: number, height: number) {
+        this.canvas.width = width;
+        this.canvas.height = height;
+        this.context.fillStyle = 'red';
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
     }
     render() {
-        this.context.fillStyle = 'red';
+        this.player.update();
         this.player.draw();
     }
 }
