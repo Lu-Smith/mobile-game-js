1345e7df310ba846d89f1652d009b1d1b20ca915
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-02 20:00:28 +0000

    Updated player.ts

--- src/player.ts
+++ src/player.ts
@@ -6,19 +6,27 @@ export default class Player {
     y: number;
     width: number;
     height: number;
+    spriteWidth: number;
+    spriteHeight: number;
 
     constructor(game: Game) {
         this.game =  game;
-        this.x = 30;
+        this.x = 50;
         this.y = 60;
-        this.width = 100;
-        this.height = 100;
+        this.spriteWidth = 200;
+        this.spriteHeight = 200;
+        this.width = 200;
+        this.height = 200;
     }
 
     draw() {
         this.game.context.fillRect(this.x, this.y, this.width, this.height);
     }
     update() {
-        this.x++;
+        // this.x++;
     } 
+    resize() {
+        this.width = this.spriteWidth * this.game.ratio;
+        this.height = this.spriteHeight * this.game.ratio;
+    }
 }
