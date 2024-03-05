d6a91e5af549c96a8ff0c15b39443f6a20971ad6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-05 12:52:16 +0000

    Updated background.ts - start working on moving and scaling background

--- src/background.ts
+++ src/background.ts
@@ -6,18 +6,29 @@ export default class Background {
     width: number;
     height: number;
     x: number;
+    scaledWidth: number;
+    scaledHeight: number;
     
     constructor(game: Game) {
         this.game = game;
         this.image = document.getElementById('background') as CanvasImageSource;
         this.width = 2400;
         this.height = this.game.baseHeight;
+       
+        this.scaledWidth = this.width;
+        this.scaledHeight = this.height;
         this.x = 0;
     }
     update(){
-
+        this.x -= this.game.speed;
+        if ( this.x <= - this.width) this.x = 0;
     }
     draw(){
-        this.game.context.drawImage(this.image, this.x, 0);
+        this.game.context.drawImage(this.image, this.x, 0, this.scaledWidth, this.scaledHeight);
+    }
+    resize() {
+        this.scaledWidth = this.width * this.game.ratio;
+        this.scaledHeight = this.height * this.game.ratio;
+        this.x = 0;
     }
 }
