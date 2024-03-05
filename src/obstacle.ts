9aef2515e30885863580b79dab579ea5c2d97dc5
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-05 15:06:40 +0000

    Updated obstacle.ts

--- src/obstacle.ts
+++ src/obstacle.ts
@@ -18,4 +18,14 @@ export default class Obstacle {
         this.x = x;
         this.y = this.game.height * 0.5 - this.scaledHeigth;
     }
+    update() {
+        this.x -= this.game.speed;
+    } 
+    draw(){
+        this.game.context.fillRect(this.x, this.y, this.scaledWidth, this.scaledHeigth);
+    }
+    resize(){
+        this.scaledWidth = this.spriteWidth * this.game.ratio;
+        this.scaledHeigth = this.spriteHeigth * this.game.ratio;
+    }
 }
