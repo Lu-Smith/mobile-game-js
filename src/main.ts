8c4edc49268635043f6bd2e63717d30221b3ad74
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-06 13:15:54 +0000

    Added timer logic to main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,13 @@ window.addEventListener('load', function() {
     if(ctx) {
       const game = new Game(canvas, ctx);
       
-      function animate() {
+      let lastTime = 0;
+      
+      function animate(timeStamp: number) {
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
         ctx?.clearRect(0, 0, canvas.width, canvas.height);
-        game.render();
+        game.render(deltaTime);
         if (!game.gameOver) requestAnimationFrame(animate);
       }
 
